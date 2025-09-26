3e3763dc50fd22538281d97ba7fb49d44b16448b
faizunnur <faizunnur71@gmail.com>
2025-09-26 21:08:01 +0600

    test: break the build to test ci

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,4 +1,5 @@
 package com.example.demo;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +9,7 @@ class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		fail("I broke this test on purpose to check my github action workflow!");
 	}
 
 }
